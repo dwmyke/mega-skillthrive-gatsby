97f2d03360064da58ff0b738bcb221365921e811
Mikel-Jon West <dwmyke@gmail.com>
2020-05-10 22:12:42 -0700

    Added Content Card, Button.. Button and hover not working

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,17 @@
 import React from "react"
-import { Container, FeatureImage } from "../components"
+import { Container, FeatureImage, Content, ContentCard } from "../components"
 
 const IndexPage = () => {
     return (
         <Container>
             <FeatureImage />
+            <Content>
+                <ContentCard 
+                    title="I don't know what's happening here"
+                    description="Lorem your Ipsum"
+                    slug="test"
+                />
+            </Content>
         </Container>
     )
 }
